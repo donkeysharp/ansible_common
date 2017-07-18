4847c65711d78b2f662bfd13022350e9d54c1281
Sergio Guillen <sergio.guillen@xtime.com>
2017-07-18 14:25:55 -0400

    Update create user script using crypt

--- others/createuser.sh
+++ others/createuser.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USER=''
-PASS=''
+USER='ansible'
+PASS='54A5ADUBB9p2M'
 GROUP='sudo'
 
-useradd $USER -d "/home/${USER}" -m -s /bin/bash -G $GROUP
-echo "${USER}:${PASS}" | chpasswd
+useradd $USER -d "/home/${USER}" -m -s /bin/bash -G $GROUP -p $PASS
+# echo "${USER}:${PASS}" | chpasswd
